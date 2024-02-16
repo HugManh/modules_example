b7ac43933102fb4fd5bc11eec6da00b3c06de26a
HugManh <manh2909@gmail.com>
2024-02-16 09:54:23 +0700

    fix: vercel config

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-
 const { imageRoute } = require("./routes")
 app.use(cors())
 
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
 app.use('/api/v1', imageRoute)
 
+// Start the server
 const PORT = 3000
-/** Run the app */
-app.listen(PORT);
-console.log("Starting server on port 3000...");
+app.listen(PORT, () => {
+    console.log("Starting server on port " + PORT + "...");
+})
 
+// Export the Express API
 module.exports = app;
