8c5ec5e77fc4ecc4bd7c5b8a2c762443d258f288
manhph <manh2909@gmail.com>
2024-04-24 10:38:49 +0700

    feat: workflows test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,9 @@
 const path = require("path")
 const api = require("./api")
-const http = require('http')
-const https = require('https')
 
 let routes = (app) => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'))
+    app.get('/dashboard', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../dist/dashboard/index.html'))
     })
     app.get('/ping', (req, res) => {
         res.status(201).json({
