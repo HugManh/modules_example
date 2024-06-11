903beb6882a31ff4ec1bad200ab623c896cef3b5
manhph <manh2909@gmail.com>
2024-06-11 09:29:29 +0700

    feat: add webpack

---
+++ src/dashboard/.babelrc.js
@@ -0,0 +1,29 @@
+const presets = [
+    [
+        '@babel/env',
+        {
+            targets: {
+                edge: '17',
+                firefox: '60',
+                chrome: '67',
+                safari: '11.1',
+            },
+            useBuiltIns: 'usage',
+        },
+    ],
+    '@babel/react'
+];
+
+const plugins = [
+    ['@babel/plugin-proposal-decorators',
+        {
+            'legacy': true
+        }
+    ],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import'
+];
+
+module.exports = {
+    presets, plugins
+};
