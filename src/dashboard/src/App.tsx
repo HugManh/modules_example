0d770787970b9dd294352b06743574582fcf9111
manhph <manh2909@gmail.com>
2025-01-09 10:54:00 +0700

    feat: create book store with zustand

--- src/dashboard/src/App.tsx
+++ src/dashboard/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import { bookStore } from './store/bookStore';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
+const App = () => {
+  const reset = bookStore((state) => state.reset);
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    reset();
+  }, [reset]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div className="App">
+      <h2>My Library Store</h2>
+      <BookForm />
+      <BookList />
+    </div>
+  );
+};
+export default App;
