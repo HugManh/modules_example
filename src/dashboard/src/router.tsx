15e0befa2504ae94bbf3234ab00ebbb1a7c3a21d
HugManh <manh2909@gmail.com>
2025-01-11 22:45:45 +0700

    feat(ui): add login page

--- src/dashboard/src/router.tsx
+++ src/dashboard/src/router.tsx
@@ -1,11 +1,20 @@
 import { createBrowserRouter } from 'react-router';
 import DashboardPage from './pages/dashboard/dashboard';
 import StoragePage from './pages/storages/storages';
+import LoginPage from './pages/login/login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <DashboardPage />,
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
   {
     path: '/dashboard',
