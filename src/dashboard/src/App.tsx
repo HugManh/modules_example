63132c9d39ddec44b56c985205739a3763bf03ac
HugManh <manh2909@gmail.com>
2025-01-12 00:40:48 +0700

    chore: add tanstack query

--- src/dashboard/src/App.tsx
+++ src/dashboard/src/App.tsx
@@ -1,7 +1,17 @@
 import { RouterProvider } from 'react-router';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
 import router from './router';
 
+const queryClient = new QueryClient()
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 export default App;
