08e35e22b5534f64b144e59ce9beeb1dee39d2c7
HugManh <manh2909@gmail.com>
2025-01-12 10:55:36 +0700

    feat(ui): add register functionality

--- src/dashboard/src/pages/register/register.tsx
+++ src/dashboard/src/pages/register/register.tsx
@@ -8,28 +8,69 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Link } from 'react-router';
+import { register } from '@/services/api/AuthService';
+import { useMutation } from '@tanstack/react-query';
+import { LoaderCircle } from 'lucide-react';
+import { useRef } from 'react';
+import { Link, useNavigate } from 'react-router';
 
 export default function RegisterPage() {
+  const navigate = useNavigate();
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      console.log('Login successful');
+      navigate('/dashboard');
+    },
+  });
+
+  const handleRegisterSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    // Process form data
+    console.log('Form Data:', { email, password });
+
+    if (!name || !email || !password) {
+      return alert('Please enter email and password!');
+    }
+
+    mutation.mutate({ name, email, password });
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle className="text-2xl">Sign Up</CardTitle>
           <CardDescription>
-            Enter your information to create an account
+            Enter your information to create an account <br />
+            {mutation.isError && (
+              <span className="text-red-500 text-sm">
+                {'Something went wrong'}
+              </span>
+            )}
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleRegisterSubmit}>
             <div className="grid flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="Max" required />
+                <Input ref={nameRef} id="name" placeholder="Max" required />
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
+                  ref={emailRef}
                   id="email"
                   type="email"
                   placeholder="m@example.com"
@@ -38,10 +79,17 @@ export default function RegisterPage() {
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
+                <Input ref={passwordRef} id="password" type="password" />
               </div>
-              <Button type="submit" className="w-full">
-                <span className="ml-2">Create an account</span>
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={mutation.isPending}
+              >
+                {mutation.isPending && (
+                  <LoaderCircle className="animate-spin" />
+                )}
+                <span>Create an account</span>
               </Button>
             </div>
             <div className="mt-4 text-center text-sm">
--- src/dashboard/src/services/api/AuthService.ts
+++ src/dashboard/src/services/api/AuthService.ts
@@ -32,3 +32,33 @@ export const login = async (data: { email: string; password: string }) => {
     }, 5000);
   });
 };
+
+export const register = async (data: {
+  name: string;
+  email: string;
+  password: string;
+}) => {
+  //   api.post('auth/login', data);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (data.email === 'manh2909@gmail.com' && data.password && data.name) {
+        resolve({
+          status: 200,
+          data: {
+            token: 'fake-jwt-token',
+            user: {
+              id: 1,
+              email: 'test@example.com',
+              name: 'Test User',
+            },
+          },
+        });
+      } else {
+        reject({
+          status: 401,
+          message: 'Invalid credentials',
+        });
+      }
+    }, 5000);
+  });
+};
